2c7c4b2fc0e2fa9d0e0a5aca99551c5c25298c7f
Filip <f.dryja1@gmail.com>
2020-05-17 18:27:38 +0200

    www

--- tabela.js
+++ tabela.js
@@ -49,23 +49,43 @@ function malPo(klucz) {
         return P[klucz]-L[klucz];
     }
 }
-var tabP = [
-	new Państwo("Polska", "Europa", 38000000, 312000),
-	new Państwo("Rosja", "Azja/Europa", 147000000, 17075000),
-	new Państwo("Ukraina", "Europa", 37300000, 603000),
-	new Państwo("Chiny", "Azja", 1420000000, 9563000),
-	new Państwo("Indie", "Azja", 1296000000, 3288000),
-	new Państwo("Turcja", "Azja/Europa", 83000000, 784000),
-	new Państwo("Kazachstan", "Azja/Europa", 18000000, 2725000),
-	new Państwo("Kanada", "Ameryka Północna", 124600000, 9985000),
-	new Państwo("Meksyk", "Ameryka Północna", 18000000, 1973000),
-	new Państwo("Brazylia", "Ameryka Południowa", 210700000, 8515000),
-	new Państwo("Kolumbia", "Ameryka Południowa", 49300000, 1140000),
-	new Państwo("Maroko", "Afryka", 34000000, 447000),
-	new Państwo("Algieria", "Afryka", 41500000, 2382000),
-	new Państwo("Australia", "Australia i Oceania", 25000000, 7687000),
-	new Państwo("Nowa Zelandia", "Australia i Oceania", 4500000, 269000),
-];
+var tabP = [];
+
+function testjson() {
+	//var text ='{"kraj": "Polska","kontynent": "Europa","powierzchnia": "38000000","populacja": "312000"}';
+	var text = '[{"kraj": "Polska","kontynent": "Europa","powierzchnia": "38000000","populacja": "312000"},{"kraj": "Rosja","kontynent": "Azja/Europa","powierzchnia": "83000000","populacja": "784000"},{"kraj": "Ukraina","kontynent": "Europa","powierzchnia": "37300000","populacja": "603000"},{"kraj": "Chiny","kontynent": "Azja","powierzchnia": "1420000000","populacja": "9563000"},{"kraj": "Indie","kontynent": "Azja","powierzchnia": "1296000000","populacja": "3288000"},{"kraj": "Turcja","kontynent": "Azja/Europa","powierzchnia": "83000000","populacja": "784000"},{"kraj": "Kazachstan","kontynent": "Azja/Europa","powierzchnia": "18000000","populacja": "2725000"},{"kraj": "Kanada","kontynent": "Ameryka Północna","powierzchnia": "124600000","populacja": "9985000"},{"kraj": "Meksyk","kontynent": "Ameryka Północna","powierzchnia": "18000000","populacja": "1973000"},{"kraj": "Brazylia","kontynent": "Ameryka Południowa","powierzchnia": "49300000","populacja": "1140000"},{"kraj": "Kolumbia","kontynent": "Ameryka Południowa","powierzchnia": "49300000","populacja": "1140000"},{"kraj": "Maroko","kontynent": "Afryka","powierzchnia": "34000000","populacja": "447000"},{"kraj": "Algieria","kontynent": "Afryka","powierzchnia": "41500000","populacja": "2382000"},{"kraj": "Australia","kontynent": "Australia i Oceania","powierzchnia": "25000000","populacja": "7687000"},{"kraj": "Nowa Zelandia","kontynent": "Australia i Oceania","powierzchnia": "4500000","populacja": "269000"}]';
+
+	var obj = JSON.parse(text);
+	//test.innerHTML = obj[1].kraj;
+	//test.innerHTML = obj.length;
+
+	for (var i = 0; i < obj.length; i++) {
+		tabP.push(new Państwo(obj[i].kraj, obj[i].kontynent, obj[i].powierzchnia, obj[i].populacja));
+	}
+	//test.innerHTML = tabP[0].kraj;
+
+}
+
+
+//var tabP = [
+//	new Państwo("Polska", "Europa", 38000000, 312000),
+//	new Państwo("Rosja", "Azja/Europa", 147000000, 17075000),
+//	new Państwo("Ukraina", "Europa", 37300000, 603000),
+//	new Państwo("Chiny", "Azja", 1420000000, 9563000),
+//	new Państwo("Indie", "Azja", 1296000000, 3288000),
+//	new Państwo("Turcja", "Azja/Europa", 83000000, 784000),
+//	new Państwo("Kazachstan", "Azja/Europa", 18000000, 2725000),
+//	new Państwo("Kanada", "Ameryka Północna", 124600000, 9985000),
+//	new Państwo("Meksyk", "Ameryka Północna", 18000000, 1973000),
+//	new Państwo("Brazylia", "Ameryka Południowa", 210700000, 8515000),
+//	new Państwo("Kolumbia", "Ameryka Południowa", 49300000, 1140000),
+//	new Państwo("Maroko", "Afryka", 34000000, 447000),
+//	new Państwo("Algieria", "Afryka", 41500000, 2382000),
+//	new Państwo("Australia", "Australia i Oceania", 25000000, 7687000),
+//	new Państwo("Nowa Zelandia", "Australia i Oceania", 4500000, 269000),
+//];
+
+
 
 function generujTab(sposóbSort) {
     tabP.sort(sposóbSort);
@@ -77,17 +97,11 @@ function generujTab(sposóbSort) {
 }
 generujTab(rosnPoAlf("kraj"));
 
-var testi = 0
-function testuj() {
-	var html = "Hello World! " + testi + " <" + json;
-	test.innerHTML = html;
-	testi++;
-}
-function testjson() {
-	//var text ='{"kraj": "Polska","kontynent": "Europa","powierzchnia": "38000000","populacja": "312000"}';
-	var text = '[{"kraj": "Polska","kontynent": "Europa","powierzchnia": "38000000","populacja": "312000"},{"kraj": "Rosja","kontynent": "Azja/Europa","powierzchnia": "83000000","populacja": "784000"},{"kraj": "Ukraina","kontynent": "Europa","powierzchnia": "37300000","populacja": "603000"},{"kraj": "Chiny","kontynent": "Azja","powierzchnia": "1420000000","populacja": "9563000"},{"kraj": "Indie","kontynent": "Azja","powierzchnia": "1296000000","populacja": "3288000"},{"kraj": "Turcja","kontynent": "Azja/Europa","powierzchnia": "83000000","populacja": "784000"},{"kraj": "Kazachstan","kontynent": "Azja/Europa","powierzchnia": "18000000","populacja": "2725000"},{"kraj": "Kanada","kontynent": "Ameryka Północna","powierzchnia": "124600000","populacja": "9985000"},{"kraj": "Meksyk","kontynent": "Ameryka Północna","powierzchnia": "18000000","populacja": "1973000"},{"kraj": "Brazylia","kontynent": "Ameryka Południowa","powierzchnia": "49300000","populacja": "1140000"},{"kraj": "Kolumbia","kontynent": "Ameryka Południowa","powierzchnia": "49300000","populacja": "1140000"},{"kraj": "Maroko","kontynent": "Afryka","powierzchnia": "34000000","populacja": "447000"},{"kraj": "Algieria","kontynent": "Afryka","powierzchnia": "41500000","populacja": "2382000"},{"kraj": "Australia","kontynent": "Australia i Oceania","powierzchnia": "25000000","populacja": "7687000"},{"kraj": "Nowa Zelandia","kontynent": "Australia i Oceania","powierzchnia": "4500000","populacja": "269000"}]';
+//var testi = 0
+//function testuj() {
+//	var html = "Hello World! " + testi + " <" + json;
+//	test.innerHTML = html;
+//	testi++;
+//}
 
-	var obj = JSON.parse(text);
-	test.innerHTML = obj[1].kraj;
-}
 testjson()
