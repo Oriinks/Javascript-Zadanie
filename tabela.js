f1fe2f05b11cc49c04328908281c8d3465992797
MDeyk <helerek@gmail.com>
2020-05-17 16:36:04 +0200

    test button 2

--- tabela.js
+++ tabela.js
@@ -74,15 +74,5 @@ function generujTab(sposóbSort) {
     }
     tabela.innerHTML = html;
 }
-
 generujTab(rosnPoAlf("kraj"));
-
-//var json = $.getJSON("dane.json");
-var json = require('data.json');
-
-var testi = 0
-function testuj() {
-	var html = "Hello World! " + testi + " <" + json;
-	test.innerHTML = html;
-	testi++;
-}
+//test
